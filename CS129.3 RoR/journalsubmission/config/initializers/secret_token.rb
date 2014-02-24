e1f0a285fe668ed4bef2719533e06b3a0ee6be51
RJ <rjncruz@gmail.com>
2014-02-24 10:51:47 +0800

    28

---
+++ CS129.3 RoR/journalsubmission/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Journalsubmission::Application.config.secret_key_base = '3c165cea9b14169dacb739fe4297fe16fdf9fd80ba3188f57a62fe5cf501258346486efcf801723511b7f95106e9d2e4e328073e53a433cab75743a4a0409132'
